9ca25ef06a51b968abcc9d388c8f38bd91dd521e
Agustin Martinez <gutin97agustin.martinez@gmail.com>
2023-03-09 15:21:27 -0300

    Corrigiendo identacion innerHTML

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const xcaja = document.getElementById( 'caja-main' )
 function crearTarjetaConInner( elementoTarjeta ){
     return `
     <div class="card" style="width: 18rem; height: 23rem;">
-    <img src="${elementoTarjeta.image}" class="card-img-top" alt="imagen-${elementoTarjeta.name}" style="height: 10rem;">
-    <div class="card-body d-flex justify-content-end gap-2 flex-column">
-    <h5 class="card-title">${elementoTarjeta.name}</h5>
-    <p class="card-text">${elementoTarjeta.description}</p>
-        <a href="./details.html" class="btn btn-primary">Check it</a>
+        <img src="${elementoTarjeta.image}" class="card-img-top" alt="imagen-${elementoTarjeta.name}" style="height: 10rem;">
+        <div class="card-body d-flex justify-content-end gap-2 flex-column">
+            <h5 class="card-title">${elementoTarjeta.name}</h5>
+            <p class="card-text">${elementoTarjeta.description}</p>
+            <a href="./details.html" class="btn btn-primary">Check it</a>
         </div>
-        </div>
-        ` 
+    </div>
+    ` 
     }
     
     function pintarTarjetas( listaData, elemento ){
